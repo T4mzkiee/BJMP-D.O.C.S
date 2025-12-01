b1947d2475527f7c7a157ada28a7e18ad9735cec
T4mzkiee <105405694+T4mzkiee@users.noreply.github.com>
2025-12-01 09:18:23 +0800

    feat: Update default user data with BJMP roles
    Replaces generic user profiles with specific BJMP personnel and departments to better reflect the application's intended use.

--- constants.ts
+++ constants.ts
@@ -24,23 +24,63 @@ export const INITIAL_USERS: User[] = [
   },
   {
     id: '2',
-    name: 'John Doe',
-    email: 'john@doctrack.com',
+    name: 'JO1 De Ocampo',
+    email: 'ord@bjmp.com',
     role: Role.USER,
     isActive: true,
-    department: 'Human Resources',
-    password: 'user123', // Default password
+    department: 'ORD',
+    password: 'user123',
     avatarUrl: 'https://picsum.photos/200/200?random=2'
   },
   {
     id: '3',
-    name: 'Jane Smith',
-    email: 'jane@doctrack.com',
+    name: 'JO1 Naputo',
+    email: 'arda@bjmp.com',
     role: Role.USER,
-    isActive: false,
-    department: 'Finance',
-    password: 'user123', // Default password
+    isActive: true,
+    department: 'ARDA',
+    password: 'user123',
     avatarUrl: 'https://picsum.photos/200/200?random=3'
+  },
+  {
+    id: '4',
+    name: 'JO1 Palita',
+    email: 'ardo@bjmp.com',
+    role: Role.USER,
+    isActive: true,
+    department: 'ARDO',
+    password: 'user123',
+    avatarUrl: 'https://picsum.photos/200/200?random=4'
+  },
+  {
+    id: '5',
+    name: 'JO1 Palita',
+    email: 'rcds@bjmp.com',
+    role: Role.USER,
+    isActive: true,
+    department: 'RCDS',
+    password: 'user123',
+    avatarUrl: 'https://picsum.photos/200/200?random=5'
+  },
+  {
+    id: '6',
+    name: 'JO1 Quejada',
+    email: 'rictmd@bjmp.com',
+    role: Role.USER,
+    isActive: true,
+    department: 'RICTMD',
+    password: 'user123',
+    avatarUrl: 'https://picsum.photos/200/200?random=6'
+  },
+  {
+    id: '7',
+    name: 'JO2 Derecho',
+    email: 'rprmd@bjmp.com',
+    role: Role.USER,
+    isActive: true,
+    department: 'RPRMD',
+    password: 'user123',
+    avatarUrl: 'https://picsum.photos/200/200?random=7'
   }
 ];
 
